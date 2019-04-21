bcd3e4d83e120369b494266953190fc80cfe4d02
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-04-22 00:32:25 +0400

    Added theme stylesheet dependencies

--- functions.php
+++ functions.php
@@ -11,9 +11,9 @@ define( 'CONJ_CHILD_THEME_VERSION', $conj_child_theme->get( 'Version' ) );
 if ( ! function_exists( 'conj_child_parent_css' ) ) :
     function conj_child_parent_css() {
 
-    	$suffix 			= 	( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) 				? 	'' 					: 	'.min';
-    	$version 			= 	( defined( 'CONJ_THEME_VERSION' ) && CONJ_THEME_VERSION )   ?   CONJ_THEME_VERSION  :   CONJ_CHILD_THEME_VERSION;
-    	$stylesheet_deps 	=  	apply_filters( 'conj_vendor_stylesheet_deps', array( 'feather', 'socicon', 'cookieBubble', 'css-devices', 'js-offcanvas', 'slinky', 'iziToast', 'flickity' ) );
+    	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ?  '' : '.min';
+    	$version = ( defined( 'CONJ_THEME_VERSION' ) && CONJ_THEME_VERSION )  ?  CONJ_THEME_VERSION  :  CONJ_CHILD_THEME_VERSION;
+    	$stylesheet_deps = (array) apply_filters( 'conj_vendor_stylesheet_deps', array( 'feather', 'socicon', 'cookieBubble', 'css-devices', 'js-offcanvas', 'slinky', 'iziToast', 'flickity' ) );
 
         wp_enqueue_style( 'conj-styles', trailingslashit( get_template_directory_uri() ) . 'style' . $suffix . '.css', $stylesheet_deps, $version );
 
