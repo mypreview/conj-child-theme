3471ed1cb00c09d639ab2590ece3755e270e20d5
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-06-21 21:11:16 +0400

    Updated stylesheet dependencies

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ if ( ! function_exists( 'conj_child_parent_css' ) ) :
 
     	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ?  '' : '.min';
     	$version = ( defined( 'CONJ_THEME_VERSION' ) && CONJ_THEME_VERSION )  ?  CONJ_THEME_VERSION  :  CONJ_CHILD_THEME_VERSION;
-    	$stylesheet_deps = (array) apply_filters( 'conj_vendor_stylesheet_deps', array( 'feather', 'socicon', 'cookieBubble', 'css-devices', 'js-offcanvas', 'slinky', 'iziToast', 'flickity' ) );
+    	$stylesheet_deps = (array) apply_filters( 'conj_vendor_stylesheet_deps', array( 'feather', 'socicon', 'cookieBubble', 'css-devices', 'js-offcanvas', 'simplebar', 'slinky', 'jquery-bugme', 'iziToast', 'flickity' ) );
 
         wp_enqueue_style( 'conj-styles', trailingslashit( get_template_directory_uri() ) . 'style' . $suffix . '.css', $stylesheet_deps, $version );
 
@@ -21,8 +21,5 @@ if ( ! function_exists( 'conj_child_parent_css' ) ) :
 endif;
 
 add_action( 'wp_enqueue_scripts', 'conj_child_parent_css', 10 );
-
-// Add support for "CONJ PowerPack" plugin.
-add_filter( 'conj_powerpack_plugin_supported', '__return_true', -1 );
 
 // END ENQUEUE PARENT ACTION
